b52627a8d08941e93a506fb0f3b3bd4fef94c791
+ <jacqueline.trapp18@gmail.com>
2024-12-11 23:27:10 -0500

    lab complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        return [w for w in word_list if set(w)==set(self.word)]
+
+listen = Anagram("listen")
+print(listen.match(['enlists', 'google', 'inlets', 'banana']))
